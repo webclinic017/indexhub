41323e262e909b101deb1e924e21ea80ea8867b3
kennysimsj <82559807+kennysimsj@users.noreply.github.com>
2023-01-13 17:36:57 +0800

    refactor: Rename `update_metadata` to `update_source`  (#31)
    ### Checklist:
    
    - [x] My pull request represents one logical piece of work
    - [x] The commit message follows [conventional
    commits](https://www.conventionalcommits.org/en/v1.0.0/)
    - [x] My code follows the recommendations in
    [conventions](https://github.com/OrcaBaseHQ/conventions)
    
    **What kind of change does this PR introduce?**
    - Remove prefect decorators for functions in `status.py`
    - Merge `update_status` and `update_metadata` into single function
    `update_source`
    
    **Where should the reviewer start / focus on?**
    - `status.py`

--- indexhub/ingestion/status.py
+++ indexhub/ingestion/status.py
@@ -1,9 +1,8 @@
 import json
 import os
 from datetime import datetime
-from typing import Any, Mapping, Optional, Union
+from typing import Any, Mapping, Union
 
-from prefect import flow, task
 from sqlmodel import Session, create_engine, select
 
 from indexhub.api.models.source import Source
@@ -20,20 +19,22 @@ def get_psql_conn_uri():
     return uri
 
 
-@task
-def update_metadata(
-    report_id: str,
-    freq: str,
-    status: str,
-    paths: Optional[Mapping[str, str]],
-    start_date: Optional[datetime] = None,
-    end_date: Optional[datetime] = None,
-    msg: Optional[str] = None,
+def update_source(
+    paths: Mapping[str, Union[str, Mapping[str, Any]]], metadata: Mapping[str, Any]
 ):
+    # Unpack the metadata
+    report_id = metadata["report_id"]
+    paths = {k: v for k, v in paths.items() if k != "metadata"}
+    freq = metadata["freq"]
+    start_date = metadata.get("start_date", None)
+    end_date = metadata.get("end_date", None)
+    status = metadata["status"]
+    msg = metadata.get("msg", None)
 
     if start_date and end_date is not None:
         start_date = datetime.combine(start_date, datetime.min.time())
         end_date = datetime.combine(end_date, datetime.min.time())
+
     # Establish connection
     engine = create_engine(get_psql_conn_uri())
 
@@ -53,27 +54,3 @@ def update_metadata(
         session.add(result)
         session.commit()
         session.refresh(result)
-
-
-@flow
-def update_status(
-    paths: Mapping[str, Union[str, Mapping[str, Any]]], metadata: Mapping[str, Any]
-):
-    # Unpack the metadata
-    report_id = metadata["report_id"]
-    paths = {k: v for k, v in paths.items() if k != "metadata"}
-    freq = metadata["freq"]
-    start_date = metadata.get("start_date", None)
-    end_date = metadata.get("end_date", None)
-    status = metadata["status"]
-    msg = metadata.get("msg", None)
-
-    update_metadata(
-        report_id=report_id,
-        paths=paths,
-        freq=freq,
-        start_date=start_date,
-        end_date=end_date,
-        status=status,
-        msg=msg,
-    )
