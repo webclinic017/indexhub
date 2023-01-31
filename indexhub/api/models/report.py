4a040957caf41b32e5dcb6beff44e715d846c8e0
Suthesan Kanagasabai <45410833+suthesank@users.noreply.github.com>
2023-01-31 13:42:42 +0800

    fix: Replace Literal type to Enum instance (#50)
    Error context: https://github.com/tiangolo/sqlmodel/issues/67

--- indexhub/api/models/report.py
+++ indexhub/api/models/report.py
@@ -1,13 +1,16 @@
 import json
 from datetime import datetime
-from typing import List, Literal, Optional
-
+import enum
+from typing import List, Optional
 from sqlmodel import Field, Relationship, SQLModel
 
 from indexhub.api.models.chart import Chart
 from indexhub.api.models.source import Source, StatusTypes  # noqa
 from indexhub.api.models.table import Table
 
+class AggMethods(str, enum.Enum):
+    SUM = "SUM"
+    MEAN = "MEAN"
 
 class Report(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True, unique=True)
@@ -33,7 +36,7 @@ class Report(SQLModel, table=True):
     completion_pct: Optional[float]
     date_features: Optional[List[str]]
     lags: Optional[List[int]]
-    agg_method: Optional[Literal["sum", "mean"]] = "sum"
+    agg_method: Optional[StatusTypes] = Field(default="SUM")
     allow_negatives: Optional[bool] = False
     use_manual_zeros: Optional[bool] = False
     msg: Optional[str]
