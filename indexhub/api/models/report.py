e1a4580aaad23a83373e43b33c0d28ace6ad4057
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-03-06 20:18:21 +0800

    feat!: Simplify `Source` and `Report` models

--- indexhub/api/models/report.py
+++ indexhub/api/models/report.py
@@ -6,7 +6,7 @@ from sqlmodel import Field, Relationship, SQLModel
 
 from indexhub.api.models.chart import Chart
 from indexhub.api.models.data_table import DataTable
-from indexhub.api.models.source import Source, StatusTypes  # noqa
+from indexhub.api.models.source import Source  # noqa
 
 
 class AggMethods(str, enum.Enum):
@@ -15,29 +15,16 @@ class AggMethods(str, enum.Enum):
 
 
 class Report(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True, unique=True)
-    source_id: Optional[int] = Field(default=None, foreign_key="source.id")
+    id: int = Field(default=None, primary_key=True, unique=True)
+    user_id: str = Field(default=None, foreign_key="user.user_id")
+    source_id: int = Field(default=None, foreign_key="source.id")
     source_name: str
-    user_id: str
-    status: Optional[StatusTypes] = Field(default="RUNNING")
     created_at: datetime
     completed_at: Optional[datetime] = None
-    charts: List[Chart] = Relationship(
-        sa_relationship_kwargs={
-            "cascade": "all, delete",
-        },
-    )
-    tables: List[DataTable] = Relationship(
-        sa_relationship_kwargs={
-            "cascade": "all, delete",
-        },
-    )
-    level_cols: List[str]
     target_col: str
-    completion_pct: Optional[float]
-    date_features: Optional[List[str]]
-    lags: Optional[List[int]]
-    agg_method: Optional[StatusTypes] = Field(default="SUM")
-    allow_negatives: Optional[bool] = False
-    use_manual_zeros: Optional[bool] = False
-    msg: Optional[str]
+    level_cols: List[str]
+    charts: List[Chart] = Relationship(sa_relationship_kwargs={"cascade": "all, delete"})
+    tables: List[DataTable] = Relationship(sa_relationship_kwargs={"cascade": "all, delete"})
+    agg_method: AggMethods = Field(default="SUM")
+    allow_negatives: bool = False
+    msg: Optional[str] = None
--- indexhub/api/models/source.py
+++ indexhub/api/models/source.py
@@ -2,33 +2,28 @@ import enum
 from datetime import datetime
 from typing import List, Optional
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, JSON
 
 from indexhub.api.models.user import User  # noqa
 
 
-class StatusTypes(str, enum.Enum):
-    SUCCESS = "SUCCESS"
-    FAILED = "FAILED"
-    RUNNING = "RUNNING"
+class SourceTypes(str, enum.Enum):
+    S3 = "S3"
+    XERO = "XERO"
 
 
 class Source(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True, unique=True)
-    user_id: Optional[str] = Field(default=None, foreign_key="user.user_id")
+    id: int = Field(default=None, primary_key=True, unique=True)
+    user_id: str = Field(default=None, foreign_key="user.user_id")
     name: str
-    s3_data_bucket: str
-    time_col: str
+    type: SourceTypes
+    created_at: datetime
+    updated_at: datetime
     freq: str
+    time_col: str
     entity_cols: List[str]
     target_cols: List[str]
-    raw_data_path: str
-    filters: Optional[str]  # Json string of Union[str, str]
-    manual_forecast_path: Optional[str]
-    fct_panel_paths: Optional[str]  # Json string of Union[str, str]
-    created_at: Optional[datetime] = None
-    status: Optional[StatusTypes] = Field(default="RUNNING")
-    updated_at: Optional[datetime] = None
+    metadata: JSON
     start_date: Optional[datetime] = None
     end_date: Optional[datetime] = None
     msg: Optional[str] = None
