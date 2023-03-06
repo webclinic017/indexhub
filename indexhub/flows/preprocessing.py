2ca4fc44c3a5bc79a2792262edba3a720aafd7ba
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-03-07 07:29:16 +0800

    feat!: Move db row update / create into flows

--- indexhub/flows/preprocessing.py
+++ indexhub/flows/preprocessing.py
@@ -1,14 +1,79 @@
 import io
+import json
 from datetime import datetime
 from hashlib import md5
-from typing import List, Mapping, Optional
+from typing import Any, List, Mapping, Optional, Union
 
 import boto3
 import polars as pl
 from functime.preprocessing import reindex_panel
+from indexhub.api.db import get_psql_conn_uri
+from indexhub.api.models.report import Report
+from indexhub.api.models.source import Source
 from prefect import flow, task
 from pydantic import BaseModel
+from sqlmodel import Session, create_engine, select
 from typing_extensions import Literal
+
+
+def update_source_row(
+    paths: Mapping[str, Union[str, Mapping[str, Any]]], metadata: Mapping[str, Any]
+):
+    # Unpack the metadata
+    source_id = metadata["source_id"]
+    paths = {k: v for k, v in paths.items() if k != "metadata"}
+    fct_panel_paths = metadata.get("fct_panel_paths", None)
+    start_date = metadata.get("start_date", None)
+    end_date = metadata.get("end_date", None)
+    status = metadata["status"]
+    msg = metadata.get("msg", None)
+
+    # Establish connection
+    engine = create_engine(get_psql_conn_uri())
+
+    with Session(engine) as session:
+        # Select rows with specific report_id only
+        statement = select(Source).where(Source.id == source_id)
+        result = session.exec(statement).one()
+        # Update the fields based on the source_id
+        result.status = status
+        result.fct_panel_paths = json.dumps(fct_panel_paths)
+        result.updated_at = datetime.utcnow()
+        result.start_date = start_date
+        result.end_date = end_date
+        result.msg = msg
+        # Add, commit and refresh the updated object
+        session.add(result)
+        session.commit()
+        session.refresh(result)
+
+
+def update_report_row(metadata: Mapping[str, Any]):
+    # Unpack metadata
+    report_id = metadata["report_id"]
+    status = metadata["status"]
+    completed_at = datetime.strptime(metadata["completed_at"], "%Y-%m-%d")
+    entities = metadata["entities"]
+    msg = metadata["msg"]
+    completion_pct = metadata["completion_pct"]
+
+    # Establish connection
+    engine = create_engine(get_psql_conn_uri())
+
+    with Session(engine) as session:
+        # Select rows with specific report_id only
+        statement = select(Report).where(Report.id == report_id)
+        result = session.exec(statement).one()
+        # Update the fields based on the report_id
+        result.status = status
+        result.completed_at = completed_at
+        result.completion_pct = completion_pct
+        result.entities = entities
+        result.msg = msg
+        # Add, commit and refresh the updated object
+        session.add(result)
+        session.commit()
+        session.refresh(result)
 
 
 class PreprocessPanelInput(BaseModel):
@@ -169,9 +234,13 @@ def preprocess_panel(inputs: PreprocessPanelInput) -> PreprocessPanelOutput:
         fct_manual_path = None
         if raw_manual_path is not None:
             # Load clean manual forecast
-            raw_manual = load_raw_panel(s3_bucket, raw_manual_path, time_col, entity_cols)
+            raw_manual = load_raw_panel(
+                s3_bucket, raw_manual_path, time_col, entity_cols
+            )
             fct_manual = raw_manual.pipe(clean_raw_panel, time_col, entity_cols)
-            fct_manual_path = export_fct_panel(fct_manual, s3_bucket, raw_manual_path, suffix="manual")
+            fct_manual_path = export_fct_panel(
+                fct_manual, s3_bucket, raw_manual_path, suffix="manual"
+            )
 
         metadata = {
             "source_id": inputs.source_id,
