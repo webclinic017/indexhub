dfd3427973485c0684ec6265dcd2874ad6b4e1c5
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-03-14 13:56:39 +0800

    feat: Move s3 logic into `services.io`

--- indexhub/api/routers/readers.py
+++ indexhub/api/routers/readers.py
@@ -1,12 +1,7 @@
 from typing import Optional
 
-import boto3
-import botocore
-from fastapi import APIRouter, HTTPException
-from indexhub.api.services.parsers import parse_csv, parse_excel, parse_parquet
-
-
-FILE_EXT_TO_PARSER = {"excel": parse_excel, "csv": parse_csv, "parquet": parse_parquet}
+from fastapi import APIRouter
+from indexhub.api.routers.readers import load_s3_data
 
 
 router = APIRouter()
@@ -14,16 +9,7 @@ router = APIRouter()
 
 @router.get("/readers/s3")
 def read_s3(s3_bucket: str, s3_path: str, file_ext: str, n_rows: Optional[int] = None):
-    s3_client = boto3.client("s3")
-    try:
-        obj = s3_client.get_object(Bucket=s3_bucket, Key=s3_path)["Body"].read()
-    except botocore.exceptions.ClientError as err:
-        error_code = err.response["Error"]["Code"]
-        if error_code == "NoSuchBucket":
-            raise HTTPException(status_code=400, detail="Invalid S3 bucket") from err
-        if error_code == "NoSuchKey":
-            raise HTTPException(status_code=400, detail="Invalid S3 path") from err
-    s3_client.close()
-    parser = FILE_EXT_TO_PARSER[file_ext]
-    data = parser(obj, n_rows)
+    data = load_s3_data(
+        s3_bucket=s3_bucket, s3_path=s3_path, file_ext=file_ext, n_rows=n_rows
+    )
     return data
