543b3653925099a31f208407d58d7615c9c2483f
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-03-15 16:09:18 +0800

    feat: Replace `get_psql_uri` with global vars

--- indexhub/api/db.py
+++ indexhub/api/db.py
@@ -1,28 +1,21 @@
 import os
-from typing import Optional
 
 from indexhub.api import models  # noqa
 from sqlmodel import SQLModel, create_engine
 
 
-def get_psql_conn_uri(
-    username: Optional[str] = None,
-    password: Optional[str] = None,
-    host: Optional[str] = None,
-    port: Optional[int] = None,
-    dbname: Optional[str] = None,
-):
-    username = username or os.environ["PSQL_USERNAME"]
-    password = password or os.environ["PSQL_PASSWORD"]
-    host = host or os.environ["PSQL_HOST"]
-    port = port or os.environ["PSQL_PORT"]
-    dbname = dbname or os.environ["PSQL_NAME"]
-    sslmode = os.environ.get("PSQL_SSLMODE", "require")
-    uri = f"postgresql://{username}:{password}@{host}:{port}/{dbname}?sslmode={sslmode}"
-    return uri
+PSQL_USERNAME = os.environ["PSQL_USERNAME"]
+PSQL_PASSWORD = os.environ["PSQL_PASSWORD"]
+PSQL_HOST = os.environ["PSQL_HOST"]
+PSQL_PORT = os.environ["PSQL_PORT"]
+PSQL_DBNAME = os.environ["PSQL_NAME"]
+PSQL_SSLMODE = os.environ.get("PSQL_SSLMODE", "require")
+PSQL_URI = (
+    f"postgresql://{PSQL_USERNAME}:{PSQL_PASSWORD}@"
+    f"{PSQL_HOST}:{PSQL_PORT}/{PSQL_DBNAME}?sslmode={PSQL_SSLMODE}"
+)
 
-
-engine = create_engine(get_psql_conn_uri(), echo=True)
+engine = create_engine(PSQL_URI, echo=True)
 
 
 def create_db_and_tables():
