211cfa37d5a104f3f67672b2995cfaf756ce19b7
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-03-15 13:04:23 +0800

    feat: Add optional psql conn args to conn getter

--- indexhub/api/db.py
+++ indexhub/api/db.py
@@ -1,15 +1,22 @@
 import os
+from typing import Optional
 
 from indexhub.api import models  # noqa
 from sqlmodel import SQLModel, create_engine
 
 
-def get_psql_conn_uri():
-    username = os.environ["PSQL_USERNAME"]
-    password = os.environ["PSQL_PASSWORD"]
-    host = os.environ["PSQL_HOST"]
-    port = os.environ["PSQL_PORT"]
-    dbname = os.environ["PSQL_NAME"]
+def get_psql_conn_uri(
+    username: Optional[str] = None,
+    password: Optional[str] = None,
+    host: Optional[str] = None,
+    port: Optional[int] = None,
+    dbname: Optional[str] = None,
+):
+    username = username or os.environ["PSQL_USERNAME"]
+    password = password or os.environ["PSQL_PASSWORD"]
+    host = host or os.environ["PSQL_HOST"]
+    port = port or os.environ["PSQL_PORT"]
+    dbname = dbname or os.environ["PSQL_NAME"]
     sslmode = os.environ.get("PSQL_SSLMODE", "require")
     uri = f"postgresql://{username}:{password}@{host}:{port}/{dbname}?sslmode={sslmode}"
     return uri
