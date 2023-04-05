20068784247ec5b150aa7fc46ddbd67c77822876
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-04-05 14:52:25 +0800

    ci: Add psql secrets and roles to stack

--- cdk/fastapi_stack.py
+++ cdk/fastapi_stack.py
@@ -1,16 +1,19 @@
-from aws_cdk import Stack
-from constructs import Construct
+import os
 
 import aws_cdk.aws_ec2 as ec2
 import aws_cdk.aws_ecs as ecs
 import aws_cdk.aws_ecs_patterns as ecs_patterns
-from aws_cdk import (
-    Stack,
-    Duration,
-    aws_ec2 as ec2,
-    aws_ecs as ecs,
-    aws_ecs_patterns as ecs_patterns,
-)
+from aws_cdk import Duration, Stack
+from aws_cdk import aws_ec2 as ec2
+from aws_cdk import aws_ecs as ecs
+from aws_cdk import aws_ecs_patterns as ecs_patterns
+from aws_cdk import aws_iam as iam
+from aws_cdk.aws_secretsmanager import Secret
+from constructs import Construct
+
+
+AWS_ACCOUNT_ID = os.environ["AWS_ACCOUNT_ID"]
+AWS_DEFAULT_REGION = os.environ["AWS_DEFAULT_REGION"]
 
 
 class FastAPIStack(Stack):
@@ -34,10 +37,104 @@ class FastAPIStack(Stack):
             # container_insights=True
         )
 
+        # SECRETS
+        psql_secret = Secret.from_secret_name_v2(
+            self, "Secret", secret_name="prod/indexhub/postgres"
+        )
+
+        # EXECUTION ROLE
+        execution_role = iam.Role(
+            self,
+            "ExecutionRole",
+            role_name="IndexHubExecutionRole",
+            assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+        )
+        execution_role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name(
+                "AmazonSSMManagedInstanceCore"
+            )
+        )
+        iam.Policy(
+            self,
+            "execution-role-policy",
+            statements=[
+                iam.PolicyStatement(
+                    effect=iam.Effect.ALLOW,
+                    actions=["logs:CreateLogStream", "logs:PutLogEvents"],
+                    resources=[f"arn:aws:logs:{AWS_DEFAULT_REGION}:{AWS_ACCOUNT_ID}:*"],
+                ),
+                iam.PolicyStatement(
+                    effect=iam.Effect.ALLOW,
+                    actions=[
+                        "secretsmanager:GetSecretValue",
+                        "secretsmanager:DescribeSecret",
+                    ],
+                    resources=[psql_secret.secret_arn],
+                ),
+            ],
+            roles=[execution_role],
+        )
+
+        # TASK ROLE
+        task_role = iam.Role(
+            self,
+            "TaskRole",
+            role_name="IndexHubTaskRole",
+            assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+        )
+        iam.Policy(
+            self,
+            "task-role-policy",
+            statements=[
+                iam.PolicyStatement(
+                    effect=iam.Effect.ALLOW,
+                    actions=["s3:GetObject", "s3:ListBucket"],
+                    resources=[
+                        "arn:aws:s3:::indexhub-feature-store",
+                        "arn:aws:s3:::indexhub-feature-store/*",
+                    ],
+                ),
+                iam.PolicyStatement(
+                    effect=iam.Effect.ALLOW,
+                    actions=[
+                        "secretsmanager:DescribeSecret",
+                        "secretsmanager:CreateSecret",
+                        "secretsmanager:GetSecretValue",
+                        "secretsmanager:PutSecretValue",
+                        "secretsmanager:TagResource",
+                    ],
+                    resources=["*"],
+                    conditions={
+                        "StringNotEquals": {"aws:ResourceTag/owner": "indexhub"},
+                        "StringEquals": {"aws:ResourceTag/owner": "user"},
+                    },
+                ),
+            ],
+            roles=[task_role],
+        )
+
+        # ECS Task
         image = ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
             image=ecs.ContainerImage.from_asset(directory="indexhub"),
             container_port=8000,
-            environment={},
+            environment={
+                "DEBUG": "false",
+                "PSQL_SSLMODE": "require",
+                "AWS_DEFAULT_REGION": AWS_DEFAULT_REGION,
+            },
+            secrets={
+                "PSQL_DBNAME": ecs.Secret.from_secrets_manager(psql_secret, "dbname"),
+                "PSQL_PORT": ecs.Secret.from_secrets_manager(psql_secret, "port"),
+                "PSQL_USERNAME": ecs.Secret.from_secrets_manager(
+                    psql_secret, "username"
+                ),
+                "PSQL_PASSWORD": ecs.Secret.from_secrets_manager(
+                    psql_secret, "password"
+                ),
+                "PSQL_HOST": ecs.Secret.from_secrets_manager(psql_secret, "host"),
+            },
+            execution_role=execution_role,
+            task_role=task_role,
         )
 
         self.ecs_service = ecs_patterns.ApplicationLoadBalancedFargateService(
--- cdk_app.py
+++ cdk_app.py
@@ -1,12 +1,14 @@
 import os
+
 from aws_cdk import App, Environment
+
 from cdk.fastapi_stack import FastAPIStack
 
 app = App()
 
 FastAPIStack(
     app,
-    "FastAPIStack",
+    "IndexHubFastAPIStack",
     env=Environment(
         account=os.environ["AWS_ACCOUNT_ID"],
         region=os.environ["AWS_DEFAULT_REGION"],
