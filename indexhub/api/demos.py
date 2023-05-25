887125023448a52f0150f8ea8757dc62f8054635
kennysimsj <82559807+kennysimsj@users.noreply.github.com>
2023-05-25 21:56:58 +0800

    feat: Add `commercial_real_estate` and `econdb_cpi` to `DEMO_SCHEMAS` (#270)

--- indexhub/api/demos.py
+++ indexhub/api/demos.py
@@ -8,5 +8,23 @@ DEMO_SCHEMAS = {
         "backtests": "commodities/artifacts/backtests__best_models.parquet",
         "quantiles": "commodities/artifacts/quantiles__best_models.parquet",
         "metadata": "commodities/metadata.json",
-    }
+    },
+    "commercial_real_estate": {
+        "entity_col": "district__grade",
+        "vectors": "vectors/commercial_real_estate.lance",
+        "y": "commercial_real_estate/artifacts/y.parquet",
+        "forecasts": "commercial_real_estate/artifacts/forecasts__best_models.parquet",
+        "backtests": "commercial_real_estate/artifacts/backtests__best_models.parquet",
+        "quantiles": "commercial_real_estate/artifacts/quantiles__best_models.parquet",
+        "metadata": "commercial_real_estate/metadata.json",
+    },
+    "econdb_cpi": {
+        "entity_col": "iso_country_code",
+        "vectors": "vectors/econdb_cpi.lance",
+        "y": "econdb_cpi/artifacts/y.parquet",
+        "forecasts": "econdb_cpi/artifacts/forecasts__best_models.parquet",
+        "backtests": "econdb_cpi/artifacts/backtests__best_models.parquet",
+        "quantiles": "econdb_cpi/artifacts/quantiles__best_models.parquet",
+        "metadata": "econdb_cpi/metadata.json",
+    },
 }
