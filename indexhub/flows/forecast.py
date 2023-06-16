16413ee990e4e9f4c48d78b3fc4f97920adfc893
jasmineliaw <chingpeng0304@live.com>
2023-06-16 13:59:33 +0800

    feat: Enable cron job for forecast flow to run in modal ui

--- indexhub/flows/forecast.py
+++ indexhub/flows/forecast.py
@@ -744,7 +744,7 @@ def run_forecast(
     memory=5120,
     cpu=8.0,
     timeout=3600,
-    # schedule=modal.Cron("0 18 * * *"),  # run at 2am daily (utc 6pm)
+    schedule=modal.Cron("0 18 * * *"),  # run at 2am daily (utc 6pm)
 )
 def schedule_forecast():
     # 1. Get all objectives
